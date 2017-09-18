cf9419457ab9aee65d41e1464cfb84812f3f4187
Cliff <tools@3csonline.com>
2017-09-18 23:11:08 +0000

    Agile Week Beginner level

---
+++ public/angular-app/buy/buy-controller.js
@@ -0,0 +1,23 @@
+angular.module('cdfinance').controller("BuyController", BuyController);
+
+function BuyController($http, $window, AuthFactory, jwtHelper, $location) {
+  var vm = this;
+  
+  vm.buy = function() {
+    if ($window.sessionStorage.token && AuthFactory.isLoggedIn) {
+      var token = $window.sessionStorage.token;
+      var decodedToken = jwtHelper.decodeToken(token);
+      var username = decodedToken.username;
+      
+      var data = {"symbol" : vm.symbol, "amount": vm.amount}
+      
+      $http.post('/api/users/'+ username +"/stocks", data).then(function(response) {
+        //check the responses
+      }).catch(function(error) {
+        console.log(error);
+      })
+    } else {
+      $location.path('/');
+    }
+  }
+}
---
+++ public/angular-app/find/find-controller.js
@@ -0,0 +1,20 @@
+angular.module('cdfinance').controller("FindController", FindController);
+
+function FindController($http) {
+  var vm = this;
+  console.log("findController");
+  vm.find = function() {
+    var symbol = vm.symbol
+    console.log(symbol)
+    
+    $http.get("/api/stocks/" + symbol).then(function(response) {
+      console.log("found stock")
+      var stockprice = response.data.price
+      vm.stockprice = stockprice;
+    }).catch(function(error) {
+      if (error) {
+        vm.error = error;
+      }
+    })
+  }
+}
