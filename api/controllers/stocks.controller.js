af755c288ba21f689f88ea73e3af01fb0cbb1e7b
joseroberts87 <webterpr@gmail.com>
2017-09-19 22:26:04 -0400

    adding a get all function to stock controller

--- api/controllers/stocks.controller.js
+++ api/controllers/stocks.controller.js
@@ -4,26 +4,45 @@ var https = require('https');
 var stockPrice = require('./shared/stockPrice.js')
 
 module.exports.stocksGetPrice = function(req, res) {
-  var symbol = req.params.symbol
-  console.log("looking up symbol:", symbol);
-  
-  Stock
-    .findById(symbol)
-    .exec(function(err, stock) {
-      if (err) {
-        res
-          .status(500)
-          .json(err);
-      } else if (!stock) {
-        res
-          .status(404)
-          .json({ "message" : "Stock symbol invalid"})
-      } else {
-        //found the stock symbol it is a valid NASDAQ stock symbol pull data
-        //from api.
-        var price = stockPrice.getPrice(req, res, symbol);
-        
-      }
-    })
-  
+    var symbol = req.params.symbol
+    console.log("looking up symbol:", symbol);
+
+    Stock
+        .findById(symbol)
+        .exec(function(err, stock) {
+            if (err) {
+                res
+                    .status(500)
+                    .json(err);
+            } else if (!stock) {
+                res
+                    .status(404)
+                    .json({ "message": "Stock symbol invalid" })
+            } else {
+                //found the stock symbol it is a valid NASDAQ stock symbol pull data
+                //from api.
+                var price = stockPrice.getPrice(req, res, symbol);
+
+            }
+        })
+
+}
+
+module.exports.getAll = function(req, res) {
+    console.log('getting all sysmbols')
+    Stock
+        .find()
+        .exec(function(err, syms) {
+            if (err) {
+                console.log(err);
+                res
+                    .status(400)
+                    .json(err);
+            } else {
+                console.log('symbols found', syms)
+                res
+                    .status(200)
+                    .json(syms);
+            }
+        })
 }
