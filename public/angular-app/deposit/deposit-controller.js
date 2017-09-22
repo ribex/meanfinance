a9a4030852e6a5bfce169fb9fc06672bd64eabe6
Nong Suon <nong.suon@gmail.com>
2017-09-22 20:31:33 +0000

    finished issue #7 display user balance after deposit

--- public/angular-app/deposit/deposit-controller.js
+++ public/angular-app/deposit/deposit-controller.js
@@ -10,9 +10,13 @@ function DepositController($http, $window, AuthFactory, jwtHelper, $location) {
       var username = decodedToken.username;
       var data = {"amount" : vm.amount};
       
+      $http.get('/api/users/' + username).then(function(response) {
+        vm.balance = response.data;
+      })
+      
       $http.put('/api/users/'+ username +"/deposit", data).then(function(response) {
         if (response.status == 200) {
-          vm.message = "Balance increased."
+          vm.message = "Balance increased to ";
         }
       }).catch(function(error) {
         console.log(error);
