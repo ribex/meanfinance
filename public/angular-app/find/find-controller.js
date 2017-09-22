f912914df4b79e0cce67fe1929420ca46066d862
joseroberts87 <webterpr@gmail.com>
2017-09-22 00:24:35 -0400

    added typeahead functions

--- public/angular-app/find/find-controller.js
+++ public/angular-app/find/find-controller.js
@@ -1,20 +1,55 @@
 angular.module('cdfinance').controller("FindController", FindController);
 
 function FindController($http) {
-  var vm = this;
-  console.log("findController");
-  vm.find = function() {
-    var symbol = vm.symbol
-    console.log(symbol)
-    
-    $http.get("/api/stocks/" + symbol).then(function(response) {
-      console.log("found stock")
-      var stockprice = response.data.price
-      vm.stockprice = stockprice;
-    }).catch(function(error) {
-      if (error) {
-        vm.error = error;
-      }
-    })
-  }
+    var vm = this;
+    console.log("findController");
+
+
+    function stocksList() {
+        return $http.get('/api/stocks').then(complete).catch(failed);
+    };
+
+    stocksList().then(function(response) {
+        sendToPage(response);
+    });
+
+    function complete(response) {
+        return response.data;
+    };
+
+    function failed(error) {
+        console.log(error.statusText);
+    };
+
+    function sendToPage(tickSymbol) {
+        var tickSymbols = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: tickSymbol
+        });
+
+        $('.typeahead').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        }, {
+            name: 'symbols',
+            source: tickSymbols
+        });
+    }
+
+    vm.find = function() {
+        var symbol = vm.symbol
+        console.log(symbol)
+
+        $http.get("/api/stocks/" + symbol).then(function(response) {
+            console.log("found stock")
+            var stockprice = response.data.price
+            vm.stockprice = stockprice;
+        }).catch(function(error) {
+            if (error) {
+                vm.error = error;
+            }
+        })
+    }
 }
