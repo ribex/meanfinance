8b548f356b650fc5af588cdb31910873ced71c72
joseroberts87 <webterpr@gmail.com>
2017-09-22 00:56:33 -0400

    added forced upper case to avoid api errors

--- public/angular-app/find/find-controller.js
+++ public/angular-app/find/find-controller.js
@@ -39,7 +39,7 @@ function FindController($http) {
     }
 
     vm.find = function() {
-        var symbol = vm.symbol
+        var symbol = vm.symbol.toUpperCase();
         console.log(symbol)
 
         $http.get("/api/stocks/" + symbol).then(function(response) {
